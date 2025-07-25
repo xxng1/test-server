6ac06a5a698a94d6037fda462d6029de563d9a4a
xxng1 <woongaa1@naver.com>
2025-07-25 12:35:30 +0900

    .

--- apiserver/apiserver/views.py
+++ apiserver/apiserver/views.py
@@ -3,6 +3,6 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 @api_view(['GET'])
 def index(request):
- data = {"result":"success", "data": [{"id":"itstudy", "name":"군계"},
+ data = {"result":"success", "data": [{"id":"itstudy", "name":"냉면"},
  {"id":"ggangpae1", "name":"아담"}]}
  return Response(data, status=status.HTTP_200_OK)
